effd03a09142d82ac3562e9ee022d7821b98def6
maninserg <manin.serg@gmail.com>
2022-06-16 17:36:47 +0300

    Add interceptor for getting only data in response

--- src/plugins/axios/interceptors.js
+++ src/plugins/axios/interceptors.js
@@ -11,8 +11,13 @@ function setParams(config) {
     return config
 }
 
+function returnData(res) {
+    console.log(res);
+    return res.data;
 
+}
 
 export default function(axios) {
     axios.interceptors.request.use(setParams);
+    axios.interceptors.response.use(returnData);
 }
